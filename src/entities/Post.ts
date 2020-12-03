5789a43b96c026adf0c7364e13e3e05f102d2f15
Jonathan Perry <jpper@umich.edu>
2020-12-03 23:31:48 +0900

    Finished through 1 hour in the tutorial (still having an issue within localhost:4000/graphql)

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field(() => Int)
   @PrimaryKey()
   id: number;
 
-  @Property({type: "date"})
+  @Field(() => String)
+  @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "text", onUpdate: () => new Date() })
   updatedAt = new Date();
 
-  @Property({type: "text"})
+  @Field()
+  @Property({ type: "text" })
   title!: string;
 }
