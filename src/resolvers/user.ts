ea9866c3d97ccb9a25cd64e8d5b75fae79592824
Jonathan Perry <jpper@umich.edu>
2020-12-04 18:42:11 +0900

    Finished video through 2 hour mark (implemented session register and login with cookie save)

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -7,6 +7,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
@@ -38,6 +39,15 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // You are not logged in
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userid });
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
@@ -91,7 +101,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -116,6 +126,11 @@ export class UserResolver {
         ],
       };
     }
+
+    // Store user id session
+    // This will set cookie on the user, keep them logged in
+    req.session.userId = user.id;
+
     return {
       user,
     };
