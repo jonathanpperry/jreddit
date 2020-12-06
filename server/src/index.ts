09e2ec4f0998000e3fd98f6da2009e925447c1e9
Jonathan Perry <jpper@umich.edu>
2020-12-06 21:57:01 +0900

    Finished video through 2.75 hours and combined 2 projects into one repository

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,7 @@ import redis from "redis";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import { MyContext } from "./types";
+import cors from "cors";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -21,7 +22,7 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
-
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
   app.use(
     session({
       name: "qid",
@@ -50,7 +51,10 @@ const main = async () => {
     context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.get("/", (_, res) => {
     res.send("hello");
