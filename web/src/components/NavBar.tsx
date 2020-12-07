4fb2fd030870c93e357ed7a0832b1d622c9a9b93
Jonathan Perry <jpper@umich.edu>
2020-12-07 14:00:57 +0900

    Implemented a navbar and conditional display of links based on log in status

---
+++ web/src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+// Chakra/Next
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+  // Data is loading
+  if (fetching) {
+    // Implicitly keep the body as empty
+    // User not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+    // User is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
